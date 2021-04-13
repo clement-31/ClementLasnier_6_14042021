20e2f005d9c3f6cf0fc8df7edfcda8ed9c6e02dc
Clement31 <clement-lasnier@orange.fr>
2021-04-14 00:37:00 +0200

    Ajout package.json et server.js

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const app = require('./app');
+
+// Permet de retourner un port valide, qu'il soit obtenu sous la forme du nombre ou d'une chaîne de caractères
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+//Renvoie les erreurs pour les traiter
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+//Construit le serveur avec le protocole http et en utilisant app.js
+const server = http.createServer(app);
+
+//Lancement du gestionnaire d'erreur
+server.on('error', errorHandler);
+
+//Lancement du serveur et affichage des informations de connexion dans la console
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen(port);
+
